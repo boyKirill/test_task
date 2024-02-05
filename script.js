51e57729c92991ce3763d663eb6ebf736edb325c
Kirill Vladimirov <k.vladimirov@yandex.ru>
2024-02-05 22:12:25 +0300

    Изменены меню, форма, заголовки, кнопки

--- script.js
+++ script.js
@@ -10,7 +10,21 @@ for (let i = 0; i < listLinks.length; i++) {
 
 		listLinks[i].classList.add("active");
 
+	})
+}
 
+let listLinks2 = document.querySelectorAll(".menu-links a");
+console.log(listLinks2)
+for (let i = 0; i < listLinks2.length; i++) {
+	listLinks2[i].addEventListener("click", () => {
+
+		let activeLink = listLinks2[i].closest('.links').querySelector('a.active')
+
+		if (activeLink) {
+			activeLink.classList.remove('active');
+		}
+
+		listLinks2[i].classList.add("active");
 
 	})
 }
